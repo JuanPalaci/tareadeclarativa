d9c099210be2ead0f69cf8c9c6c554b521365921
Jacobo0991 <00005822@uca.edu.sv>
2025-08-25 16:39:48 -0600

    Base de conocimientos para actividad clase S3C1

---
+++ Actividad2-S3c1/base_conocimiento.pl
@@ -0,0 +1,47 @@
+
+aldea(rivoria).
+aldea(montelar).
+aldea(durania).
+capital(rivoria).
+
+parejas(esteban, mariela).
+parejas(andres, clara).
+parejas(rodolfo, teresa).
+parejas(ismael, hector).
+parejas(alonso, beatriz).
+parejas(andres, catalina).
+parejas(mateo, irene).
+
+origen(esteban, rivoria).
+origen(mariela, rivoria).
+origen(andres, rivoria).
+origen(clara, rivoria).
+origen(rodolfo, rivoria).
+origen(teresa, rivoria).
+origen(ismael, rivoria).
+origen(hector, rivoria).
+origen(sofia, rivoria).
+origen(lucas rivoria).
+origen(martina, rivoria).
+origen(alonso, montelar).
+origen(beatriz, montelar).
+origen(felipe, montelar).
+origen(catalina, montelar).
+origen(andres, montelar).
+origen(emilia, montelar).
+origen(mateo, durania).
+origen(irene, durania).
+
+ocupacion(andres, comerciante).
+ocupacion(clara, maestra).
+ocupacion(rodolfo, agricultor).
+ocupacion(teresa, tejedora).
+ocupacion(hector, herrero).
+ocupacion(lucas, herrero).
+ocupacion(felipe, explorador).
+ocupacion(catalina, curandera).
+ocupacion(emilia, alfarera).
+ocupacion(mateo, agricultor).
+ocupacion(irene, granjera).
+
+alianza(rivoria, durania).
