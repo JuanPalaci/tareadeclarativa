d71b9960935b3f9fccc072c30ca62e9b8634e604
Juan Carlos Sibrian <00057522@uca.edu.sv>
2025-09-03 16:59:43 -0600

    Parte I

--- Actividad2-S4c2/guia.pl
+++ Actividad2-S4c2/guia.pl
@@ -1,9 +1,23 @@
-% 1
+%Ejercicio 1 Area y perimetro de un rectangulo
+rectanguloA(X,Y, Resultado) :-
+ Resultado is  X*Y.    
 
-% 2
+rectanguloP(X,Y, Resultado) :-
+ Resultado is  X*Y.
+%Ejercicio 2 Numero par e impar
+parImpar(X,Modulo) :-
+    Modulo is X mod 2,
+    (Modulo == 0 -> write('Es par');write('es impar') ).
 
-% 3
+%Ejercicio 3 tabla de multiplicar
+tabla_mul(X) :-
+    tabla_mul(X, 1).
 
-% 4
+tabla_mul(_, A) :-
+    A > 10, !.
 
-% 5
+tabla_mul(X, A) :-
+    Resultado is X * A,
+    write(Resultado), nl,
+    NuevoA is A + 1,
+    tabla_mul(X, NuevoA).
